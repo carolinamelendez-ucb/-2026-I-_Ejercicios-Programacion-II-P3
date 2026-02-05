a5ddae39c89f8870630bebc30bfca3e98286fe85
mateoriveros <mateo.riveros@ucb.edu.bo>
2026-02-05 08:44:43 -0400

    Add files via upload

---
+++ Diagnostico.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+using namespace std;
+void ejercicio1()
+{
+    int num {13, 22, 5, 8, 34, 55, 60};
+    int par = 0;
+    for (int i = 0; i < num[i]; i++)
+    {
+        if(num[i] / 2 = 0)
+        {
+
+        }
+    }
+}
+void ejercicio2()
+{
+
+}
+void ejercicio3()
+{
+
+}
+void ejercicio4()
+{
+
+}
+
+int main()
+{
+    cout<<"";
+}
+
