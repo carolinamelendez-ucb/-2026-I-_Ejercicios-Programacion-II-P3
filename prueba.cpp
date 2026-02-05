2689e74ddbe70e3cc82f078b7f99107c61579b9f
isabel-ayza <nelly.ayza@ucb.edu.bo>
2026-02-05 08:57:20 -0400

    Nelly Ayza - Diagnostico

--- prueba.cpp
+++ prueba.cpp
@@ -1,19 +1,68 @@
-// prueba.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
 
+//Nelly Ayza
 #include <iostream>
+using namespace std;
+
+void ejercicio1(int v[])
+{
+    cout << "numeros pares del vector: ";
+    for (int i = 0;i < 7;i++)
+    {
+        if (v[i] % 2 == 0)
+        {
+            cout << v[i] << " ";
+        }
+    }
+   
+}
+
+void ejercicio2(int vec[])
+{
+    int total = 0;
+    for (int i = 0;i < 7;i++)
+    {
+        total += vec[i];
+    }
+    cout << "El total de la suma del vector es: " << total << endl;
+
+}
+void mostrarmat(int m[3][3])
+{
+
+    for (int i = 0;i < 3;i++)
+    {
+        for (int j = 0;j < 3;j++)
+        {
+            cout << "matriz [" << i << "][" << j << "]: " << m[i][j] << endl;
+        }
+    }
+}
+void ejercicio3(int m[3][3])
+{
+    for (int i = 0;i < 3;i++)
+    {
+        for (int j = 0;j < 3;j++)
+        {
+            m[i][j] = i * j;
+        }
+    }
+
+    mostrarmat(m);
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int numeros[] = {13,22,5,8,34,55,60};
+    int matriz[3][3] = { 0 };
+
+    cout << "- EJERCICIO 1 -" << endl;
+    ejercicio1(numeros);
+    cout << "\n- EJERCICIO 2 -" << endl;
+    ejercicio2(numeros);
+    cout << "\n- EJERCICIO 3 -" << endl;
+    ejercicio3(matriz);
+    //ejercicio4();
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
