88035ad4ec58c80d6d70b796b200763ef853934f
GadielArcee <leonardo.arce@ucb.edu.bo>
2026-02-10 08:48:17 -0400

    Add files via upload

---
+++ Practica1-SesionB.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Cuenta {
+private:
+    string titular;
+    double cantidad;
+
+public:
+    // Constructor con titular
+    Cuenta(string titular) {
+        this->titular = titular;
+        this->cantidad = 0.0;
+    }
+
+    
+    Cuenta(string titular, double cantidad) {
+        this->titular = titular;
+        if (cantidad >= 0)
+            this->cantidad = cantidad;
+        else
+            this->cantidad = 0.0;
+    }
+
+    // Getters
+    string getTitular() {
+        return titular;
+    }
+
+    double getCantidad() {
+        return cantidad;
+    }
+
+    // Setters
+    void setTitular(string titular) {
+        this->titular = titular;
+    }
+
+    void setCantidad(double cantidad) {
+        if (cantidad >= 0)
+            this->cantidad = cantidad;
+    }
+
+    // Mostrar datos
+    void mostrar() {
+        cout << "Titular: " << titular << endl;
+        cout << "Cantidad: " << cantidad << endl;
+    }
+
+    // Ingresar dinero
+    void ingresar(double cantidad) {
+        if (cantidad > 0) {
+            this->cantidad += cantidad;
+        }
+    }
+
+    // Retirar dinero
+    void retirar(double cantidad) {
+        if (this->cantidad - cantidad < 0) {
+            this->cantidad = 0;
+        }
+        else {
+            this->cantidad -= cantidad;
+        }
+    }
+};
+
+int main() {
+  
+    Cuenta c1("Juan Perez");
+    c1.mostrar();
+    cout << "------------------" << endl;
+
+   
+    c1.ingresar(500.50);
+    c1.mostrar();
+    cout << "------------------" << endl;
+
+    c1.retirar(200);
+    c1.mostrar();
+    cout << "------------------" << endl;
+
+    c1.retirar(500);
+    c1.mostrar();
+    cout << "------------------" << endl;
+
+    
+    Cuenta c2("Lopez", 1000);
+    c2.mostrar();
+
+    return 0;
+}
