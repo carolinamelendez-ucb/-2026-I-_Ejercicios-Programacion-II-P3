4d8afb3853e059600086fcba11ee43c51f209597
Brian-Rti <Brian.teran@ucb.edu.bo>
2026-03-03 08:50:31 -0400

    Add files via upload
    bueno tengo que practicar en mi casa por temas de no entender algunas funciones

---
+++ trabajoencurso0.3.cpp
@@ -0,0 +1,32 @@
+// trabajoencurso0.3.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+#include <string>
+#include"pasajero.h"
+using namespace std;
+void Notienenasiento() {
+
+};
+int main()
+{
+	int s = 5;
+	Pasajerosvector mispasajeros = Pasajerosvector(50);
+	mispasajeros.agregar(new Pasajero("mateo", 1243));
+	mispasajeros.agregar(new Pasajero("lucas", 123));
+	mispasajeros.agregar(new Pasajero("carlos", 1235));
+	for (int i = 0;i < 5;i++) {
+		mispasajeros.mostrar(s);
+	}
+
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
