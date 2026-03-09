858485ca469dd87580d7fb47db626f3aa9ced678
haroldosinaga-prog <harold.osinaga@ucb.edu.bo>
2026-03-09 09:33:51 -0400

    Add files via upload

---
+++ Socios.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Socio
+{
+private:
+	string nombre;
+	int ci;
+	int telefono;
+public:
+	Socio(){}
+	Socio(string nombre, int ci, int telefono)
+	{
+		this->nombre = nombre;
+		this->ci = ci;
+		this->telefono=telefono;
+	}
+
+	void mostrar()
+	{
+		cout << "Nombre: " << nombre << endl;
+		cout << "Ci: " << ci << endl;
+		cout << "Telefono: " << telefono << endl;
+	}
+	string getNombre()
+	{
+		return nombre;
+	}
+
+};
+
