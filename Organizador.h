9f8dce9c2b4f1fb0111f17f2aeae326d12868956
haroldosinaga-prog <harold.osinaga@ucb.edu.bo>
2026-03-12 08:22:22 -0400

    Add files via upload

---
+++ Organizador.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <random>
+using namespace std;
+
+class Organizador
+{
+private:
+	string nombre;
+	int id;
+	int rank;
+	bool disponibilidad;
+public:
+	Organizador(){}
+	Organizador(string nombre)
+	{
+		this->nombre = nombre;
+		this->id = rand();
+		this->rank = 0;
+		this->disponibilidad = true;
+	}
+	~Organizador()
+	{
+
+	}
+	void mostrar()
+	{
+		cout << "nombre: "<<nombre << endl;
+		cout << "Id: " <<id<< endl;
+		cout << "Rank: " << rank << endl;
+		cout << "Disponible: " << disponibilidad << endl;
+	}
+	int getRank()
+	{
+		return rank;
+	}
+	bool getDisponibilidad()
+	{
+		return disponibilidad;
+	}
+	
+};
+
