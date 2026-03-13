e67c74dcd32301e674281dda364a77107351c8c0
isaacrojast-ops <isaac.rojas.t@ucb.edu.bo>
2026-03-13 19:32:04 -0400

    Add files via upload

---
+++ invitado.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class Invitado {
+private:
+	string nombre;
+	int telefono;
+public:
+	Invitado(string nombre, int telefono) {
+		this->nombre = nombre;
+		this->telefono = telefono;
+	}
+	void mostrarInvitado() {
+		cout << "Nombre: " << nombre << endl;
+		cout << "Telefono: " << telefono << endl;
+	}
+};
