85560cb6046e13e7aca5b1176ab3567a66714c1b
benjaminsoto291 <benjamin.soto@ucb.edu.bo>
2026-03-31 08:25:28 -0400

    Add files via upload

---
+++ Animales.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class Animal {
+public:
+    virtual void hacerSonido() {
+        cout << "El animal hace un sonido" << endl;
+    }
+};
+class Perro : public Animal {
+public:
+    void hacerSonido() override {
+        cout << "guau guau" << endl;
+    }
+};
+class Gato : public Animal {
+public:
+    void hacerSonido() override {
+        cout << "miau miau" << endl;
+    }
+};
+
+int main() {
+    Animal* animales[3];
+    animales[0] = new Perro();
+    animales[1] = new Gato();
+    animales[2] = new Perro();
+
+    for (int i = 0; i < 3; i++) {
+        animales[i]->hacerSonido();
+    }
+}
