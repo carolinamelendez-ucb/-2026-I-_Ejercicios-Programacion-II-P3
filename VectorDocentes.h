7e3b8b263287121ce8b2ff32fc0f4f745d1c1269
Dania Cano <dania.cano@ucb.edu.bo>
2026-04-15 23:50:43 -0400

    Add files via upload

---
+++ VectorDocentes.h
@@ -0,0 +1,53 @@
+#pragma once
+#include "Docente.h"
+
+class VectorDocentes {
+private:
+	int indice;
+	int tamanio;
+	Docente** arrayDocentes;
+public:
+	VectorDocentes(int tamanio) {
+		this->indice = 0;
+		this->tamanio = tamanio;
+		this->arrayDocentes = new Docente * [tamanio];
+	}
+
+	void agregar(Docente* nuevoDocente) {
+		if (indice < tamanio) {
+			arrayDocentes[indice] = nuevoDocente;
+			indice++;
+		}
+	}
+	Docente*buscarPorCi(string ciBuscado) {
+		for (int i = 0; i < indice; i++) {
+			if (arrayDocentes[i]->getCi() == ciBuscado) {
+				return arrayDocentes[i];
+			}
+		}
+
+		return nullptr;
+	}
+	void mostrar() {
+		for (int i = 0; i < indice; i++) {
+			arrayDocentes[i]->mostrar();
+			cout << "------------------" << endl;
+		}
+	}
+
+	Docente* docenteQueMenosGana() {
+		Docente* menor = nullptr;
+		for (int i = 1; i < indice; i++) {
+			if (menor == nullptr || arrayDocentes[i]->calcularSalario() < menor->calcularSalario()) {
+				menor = arrayDocentes[i];
+			}
+		}
+		return menor;
+	}
+	~VectorDocentes() {
+		for (int i = 0; i < indice; i++) {
+			delete arrayDocentes[i];
+		}
+		delete[] arrayDocentes;
+	}
+};
---
+++ VectorEstudiantes.h
@@ -0,0 +1,60 @@
+#pragma once
+#include "Estudiante.h"
+
+class VectorEstudiantes {
+private:
+	int indice;
+	int tamanio;
+	Estudiante** arrayEstudiantes;
+public:
+	VectorEstudiantes(int tamanio) {
+		this->indice = 0;
+		this->tamanio = tamanio;
+		this->arrayEstudiantes = new Estudiante * [tamanio];
+	}
+
+	void agregar(Estudiante* nuevoEstudiante) {
+		if (indice < tamanio) {
+			arrayEstudiantes[indice] = nuevoEstudiante;
+			indice++;
+		}
+		else {
+			cout << "Curso esta lleno" << endl;
+		}
+	}
+
+	Estudiante* buscarPorCi(string ciBuscado) {
+		for (int i = 0; i < indice; i++) {
+			if (arrayEstudiantes[i]->getCi() == ciBuscado) {
+				return arrayEstudiantes[i];
+			}
+		}
+
+		return nullptr;
+	}
+
+	void mostrar() {
+		for (int i = 0; i < indice; i++) {
+			arrayEstudiantes[i]->mostrar();
+		}
+	}
+
+	Estudiante* estudianteQueMasPaga() {
+	
+		Estudiante* mayor = nullptr;
+
+		for (int i = 1; i < indice; i++) {
+			if (mayor== nullptr || arrayEstudiantes[i]->calcularMatricula() > mayor->calcularMatricula()) {
+				mayor = arrayEstudiantes[i];
+			}
+		}
+
+		return mayor;
+	}
+	~VectorEstudiantes() {
+		for (int i = 0; i < indice; i++) {
+			delete arrayEstudiantes[i];
+		}
+		delete[] arrayEstudiantes;
+	}
+};
